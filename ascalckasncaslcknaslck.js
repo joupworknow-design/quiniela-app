79c1567fbe0a698345a174f3ab23fae0b589dce9
systems-multimedia <samuelboada@gmail.com>
2026-06-17 14:05:39 -0400

    feat: integrate Firebase authentication and Firestore for admin panel, update UI elements, and enhance data handling with new user management features.

---
+++ ascalckasncaslcknaslck.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAR9vHk2SwvCwqXQ0P2gIOhAeJl7OBjT8o",
+  authDomain: "quiniela-jcwc-2026.firebaseapp.com",
+  projectId: "quiniela-jcwc-2026",
+  storageBucket: "quiniela-jcwc-2026.firebasestorage.app",
+  messagingSenderId: "466542252250",
+  appId: "1:466542252250:web:93613408554d2bff8695e3",
+  measurementId: "G-49GPWKPTJV",
+};
